2c3deb163290a8d4d797e82411b26eb33a0c30df
xiaojianggegea <2314398223@qq.com>
2021-05-14 16:41:35 +0800

    04代理模式

---
+++ 04_AgencyModel/src/com/ghj/test02/Main.java
@@ -0,0 +1,22 @@
+package com.ghj.test02;
+
+import com.ghj.test02.entity.Mama;
+import com.ghj.test02.entity.Proxy;
+
+/**
+ * @program: 04_AgencyModel
+ * @description: 客户端类
+ * @author: 小江
+ * @create: 2021-05-14 15:31
+ **/
+
+public class Main {
+    public static void main(String[] args) {
+        Mama schoolGirl = new Mama();
+        schoolGirl.setName("鱼儿");
+        Proxy proxy = new Proxy(schoolGirl);
+        proxy.giveClothes();
+        proxy.giveFlowers();
+        proxy.giveBag();
+    }
+}
---
+++ 04_AgencyModel/src/com/ghj/test03/prot/PursuitImpl.java
@@ -0,0 +1,33 @@
+package com.ghj.test03.prot;
+
+import com.ghj.test03.entity.Mama;
+
+/**
+ * @program: 04_AgencyModel
+ * @description: 儿子类
+ * @author: 小江
+ * @create: 2021-05-14 16:20
+ **/
+
+public class PursuitImpl implements GiveGift {
+    Mama	mm;
+    public PursuitImpl(Mama mm)
+    {
+        this.mm = mm;
+    }
+
+    @Override
+    public void giveClothes() {
+        System.out.println(mm.name + " 送你衣服");
+    }
+
+    @Override
+    public void giveFlowers() {
+        System.out.println(mm.name + " 送你鲜花");
+    }
+
+    @Override
+    public void giveBag() {
+        System.out.println(mm.name + " 送你包包");
+    }
+}
